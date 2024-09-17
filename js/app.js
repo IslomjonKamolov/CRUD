f38aabd56f0986da60d3d3417af951881a6daf56
IslomjonKamolov <ikamolov2008@gmail.com>
2024-09-17 19:26:44 +0500

    Ma'nosiz gapdan ma'no izlama

--- js/app.js
+++ js/app.js
@@ -17,27 +17,33 @@ window.addEventListener('keyup', (e) => {
     }
 })
 
-
-
 const title = document.querySelector('#title')
 const cTitle = document.querySelector('#C_title')
 const content = document.querySelector('#textarea')
-const form = document.querySelector('form')
+const form = document.querySelector('.form')
 const addBtn = document.querySelector('.draft__add__button')
 const texts__box = document.querySelector('.titles__box')
 const saved = document.querySelector('.saved')
 const draftFace = document.querySelector('.draftFace')
 const draftTitle = document.querySelector('.draftTitle')
 const draftContent = document.querySelector('.draftContent')
-
+const editButton = document.querySelectorAll('.editButton')
+const editTitle = document.querySelector('#editTitle')
+const editT = document.querySelector('#E_title')
+const editA = document.querySelector('#Etextarea')
+const editForm = document.querySelector('.forEdit')
 
 let function_varible = 'add'
+let dataBase = []
+
+if (localStorage.getItem('info')) {
+    dataBase = JSON.parse(localStorage.getItem('info'))
+    updateDOM()
+}
 
 addBtn.addEventListener('click', () => {
-    console.log('tap')
-    if (function_varible == 'add') {
+    if (function_varible === 'add') {
         function_varible = 'close'
-
         addBtn.src = './icons/exit.svg'
         Object.assign(texts__box.style, {
             opacity: '0',
@@ -48,7 +54,7 @@ addBtn.addEventListener('click', () => {
                 opacity: '1',
                 transform: 'scale(1)',
             })
-        }, 300);
+        }, 300)
         setTimeout(() => {
             Object.assign(texts__box.style, {
                 display: 'none'
@@ -56,7 +62,7 @@ addBtn.addEventListener('click', () => {
             Object.assign(form.style, {
                 display: 'flex',
             })
-        }, 200);
+        }, 200)
     } else {
         function_varible = 'add'
         addBtn.src = './icons/add.svg'
@@ -64,7 +70,6 @@ addBtn.addEventListener('click', () => {
             opacity: '0',
             transform: 'scale(0)',
         })
-
         Object.assign(form.style, {
             opacity: '0',
             transform: 'scale(0)',
@@ -77,41 +82,81 @@ addBtn.addEventListener('click', () => {
             Object.assign(texts__box.style, {
                 display: 'flex'
             })
-        }, 200);
+        }, 200)
         setTimeout(() => {
             Object.assign(texts__box.style, {
                 opacity: '1',
                 transform: 'scale(1)',
             })
-        }, 300);
+        }, 300)
     }
 })
-let dataBase = []
-if (localStorage.getItem('info')) {
-    dataBase = JSON.parse(localStorage.getItem('info'))
-    dataBase.forEach((item) => {
+
+// Funktsiyani yangilash va DOMni qayta tuzish
+function updateDOM() {
+    texts__box.innerHTML = ''
+    dataBase.forEach((item, index) => {
         let titleBox = document.createElement('div')
         titleBox.classList.add('title__box')
         titleBox.innerHTML = `
-                <h3 class="main__title">${item.title}</h3>
-                <div class="menuBar">
+            <h3 class="main__title">${item.title}</h3>
+            <div class="menuBar">
                 <img class="editButton" src="./icons/pencil.svg" alt="edit">
-                     <img class="trashBin" src="./icons/trashBIn.svg" alt="trash bin">
-                </div> 
-                `
-        document.querySelector('.titles__box').appendChild(titleBox)
+                <img class="trashBin" src="./icons/trashBin.svg" alt="trash bin">
+            </div> 
+        `
+        texts__box.appendChild(titleBox)
     })
+
+    addEvent()
+    empty()
 }
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (title.value.length > 25) {
+        alert('Title should not exceed 25 characters')
+        return
+    }
+
+    dataBase.push({
+        title: title.value,
+        cTitle: cTitle.value,
+        content: content.value,
+        id: new Date().getTime(),
+    })
+
+    Object.assign(saved.style, {
+        opacity: '0.0',
+        display: 'flex',
+    })
+    setTimeout(() => {
+        Object.assign(saved.style, {
+            opacity: '0.6',
+        })
+    }, 100)
+    setTimeout(() => {
+        Object.assign(saved.style, {
+            opacity: '0.0',
+        })
+        setTimeout(() => {
+            saved.style.display = 'none'
+        }, 100)
+    }, 2000)
+
+    form.reset()
+    localStorage.setItem('info', JSON.stringify(dataBase))
+    updateDOM()
+})
 
 function addEvent() {
     const myDraftListItems = document.querySelectorAll('.title__box')
     const menuBar = document.querySelectorAll('.menuBar')
+
     myDraftListItems.forEach((draft, index) => {
         draft.addEventListener('click', (e) => {
-            console.dir('START!')
             if (e.target.className == 'main__title') {
-                const selectedItem = dataBase.find(data => data.id === dataBase[index].id)
+                const selectedItem = dataBase[index]
                 draftFace.style.display = 'flex'
                 texts__box.style.display = 'none'
                 function_varible = 'close'
@@ -123,34 +168,52 @@ function addEvent() {
                         opacity: '1',
                         transform: 'scale(1)',
                     })
-                }, 100);
-                console.log(selectedItem);
+                }, 100)
+            } else if (e.target.classList.contains('editButton')) {
+                editForm.style.display = 'flex'
+                texts__box.style.display = 'none'
+                let editingItem = dataBase[index]
+                editTitle.value = editingItem.title
+                editT.value = editingItem.cTitle
+                editA.value = editingItem.content
+
+                // `submit` event oldingi hodisalar bilan aralashib ketmasligi uchun uni bir marta qo'shish kerak
+                editForm.onsubmit = (e) => {
+                    e.preventDefault()
+
+                    // Ma'lumotlarni yangilash
+                    editingItem.title = editTitle.value
+                    editingItem.cTitle = editT.value
+                    editingItem.content = editA.value
+
+                    texts__box.style.display = 'flex'
+
+                    // LocalStorage-ni yangilash
+                    localStorage.setItem('info', JSON.stringify(dataBase))
+
+                    // DOMni yangilash
+                    updateDOM()
+
+                    // Formani yopish
+                    editForm.reset()
+                    editForm.style.display = 'none'
+                }
+
+                // Bekor qilish tugmasi orqali formani yopish
+                editForm.querySelector('button[type=button]').addEventListener('click', () => {
+                    editForm.style.display = 'none'
+                    texts__box.style.display = 'flex'
+                    editForm.reset()
+                })
             } else if (e.target.className == 'trashBin') {
-                const deletingItem = dataBase.find(data => data.id === dataBase[index].id)
-                let newDB = dataBase.filter((db) => {
-                    return db.id != deletingItem.id
-                })
-                dataBase = newDB
-                texts__box.innerHTML = ``
-                dataBase.forEach((item, index) => {
-                    let titleBox = document.createElement('div')
-                    titleBox.classList.add('title__box')
-                    titleBox.innerHTML = `
-                            <h3 class="main__title">${item.title}</h3>
-                            <div class="menuBar">
-                            <img class="editButton" src="./icons/pencil.svg" alt="edit">
-                                 <img class="trashBin" src="./icons/trashBIn.svg" alt="trash bin">
-                            </div> 
-                            `
-                    document.querySelector('.titles__box').appendChild(titleBox)
-                })
-                addEvent()
-                if(dataBase.length < 1){
+                const deletingItem = dataBase[index]
+                dataBase = dataBase.filter((db) => db.id != deletingItem.id)
+                if (dataBase.length < 1) {
                     empty()
                 }
+                localStorage.setItem('info', JSON.stringify(dataBase))
+                updateDOM()
             }
-            localStorage.setItem('info', JSON.stringify(dataBase))
-            console.log('DONE!');
         })
 
         draft.addEventListener('contextmenu', (e) => {
@@ -158,10 +221,10 @@ function addEvent() {
             menuBar.forEach(bar => {
                 bar.style.opacity = '0'
             })
-            menuBar[index].style.display = "flex"
+            menuBar[index].style.display = 'flex'
             setTimeout(() => {
                 menuBar[index].style.opacity = '1'
-            }, 100);
+            }, 100)
         })
 
         window.addEventListener('click', () => {
@@ -169,99 +232,38 @@ function addEvent() {
                 bar.style.display = 'none'
             })
         })
-
     })
 }
-
-addEvent()
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if (title.value.length > 25) {
-        alert('Title should not exceed 25 characters')
-        return;
-    } else {
-
-        dataBase.push({
-            title: title.value,
-            cTitle: cTitle.value,
-            content: content.value,
-            id: new Date().getTime(),
-        })
-
-
-        Object.assign(saved.style, {
-            opacity: '0.0',
-            display: 'flex',
-        })
-        setTimeout(() => {
-            Object.assign(saved.style, {
-                opacity: '0.6',
-            })
-        }, 100);
-        setTimeout(() => {
-            Object.assign(saved.style, {
-                opacity: '0.0',
-            })
-            setTimeout(() => {
-                saved.style.display = 'none'
-            }, 100);
-        }, 2000);
-    }
-
-
-    document.querySelector('.titles__box').innerHTML = ''
-
-    dataBase.forEach((item, index) => {
-        let titleBox = document.createElement('div')
-        titleBox.classList.add('title__box')
-        titleBox.innerHTML = `
-                <h3 class="main__title">${item.title}</h3>
-                <div class="menuBar">
-                <img class="editButton" src="./icons/pencil.svg" alt="edit">
-                     <img class="trashBin" src="./icons/trashBIn.svg" alt="trash bin">
-                </div> 
-                `
-        document.querySelector('.titles__box').appendChild(titleBox)
-        addEvent()
-        console.log('done')
-    })
-    localStorage.setItem('info', JSON.stringify(dataBase))
-    form.reset()
-})
 
 
 
 function empty() {
-    if (document.querySelector('.titles__box').innerHTML == '') {
-        document.querySelector('.titles__box').innerHTML = `<p class="no__data__text">No data available</p>`
+    if (texts__box.innerHTML === '') {
+        texts__box.innerHTML = `<p class="no__data__text">No data available</p>`
     }
 }
 
 empty()
 
-
-
 secretInp.addEventListener('keyup', (e) => {
-    if (e.keyCode == 13) {
+    if (e.keyCode === 13) {
         secretInp.style.display = 'none'
-        if (secretInp.value == '/clearStorage') {
+        if (secretInp.value === '/clearStorage') {
             localStorage.clear('info')
             dataBase = []
             texts__box.innerHTML = '<p class="no__data__text">No data available</p>'
-        }
-        if (secretInp.value == '/iamangry') {
+        } else if (secretInp.value === '/iamangry') {
             document.body.style.background = 'red'
-        }
-        if (secretInp.value == '/iamnormal') {
+        } else if (secretInp.value === '/iamnormal') {
             document.body.style.background = '#2a2f44'
-        }
-        if (secretInp.value == '/iamsad') {
+        } else if (secretInp.value === '/iamsad') {
             document.body.style.background = 'green'
         }
         secretInp.value = ''
     }
 })
 
+document.querySelector('.display__size').textContent = window.innerWidth + 'px'
 window.addEventListener('resize', () => {
-  console.log(window.innerWidth);
+    document.querySelector('.display__size').textContent = window.innerWidth + 'px'
 })
